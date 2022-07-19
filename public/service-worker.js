9e7314ca9a70dac92220932858161c62583ef33d
Omar Wright <98981985+omarwright07@users.noreply.github.com>
2022-07-18 20:17:48 -0500

    serverworker.js is working and tested

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,50 +1,31 @@
-const FILES_TO_CACHE = [
-    "./index.html",
-    "./css/style.css",
-    "./js/idb.js",
-    "./js/index.js"
-];
-
 const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'v1';
 const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./manifest.json",
+    "./css/styles.css",
+    "./js/index.js",
+    "./js/idb.js"
+];
 
-// Installation
+// Used to Register the Service Worker
 self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
+        caches.open(CACHE_NAME)
+        .then(function (cache) {
             console.log('installing cache : ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
         })
     )
 })
 
-//Activation
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheKeeplist = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            });
-            cacheKeeplist.push(CACHE_NAME);
-
-            return Promise.all(
-                keyList.map(function (key, i) {
-                    if (cacheKeeplist.indexOf(key) === -1) {
-                        console.log('deleting cache : ' + keyList[i]);
-                        return caches.delete(keyList[i]);
-                    }
-                })
-            );
-        })
-    );
-});
-
-//Waiting/Idle
+// Used to intercept request to check for the file or data in the cache
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
-        caches.match(e.request).then(function (request) {
+        caches.match(e.request)
+        .then(function (request) {
             if (request) { // if cache is available, respond with cache
                 console.log('responding with cache : ' + e.request.url)
                 return request
@@ -55,3 +36,26 @@ self.addEventListener('fetch', function (e) {
         })
     )
 })
+
+// Triggers when the service worker activates
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys()
+        .then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+        // Claims Newly Installed SW
+        .then(() => self.clients.claim())
+    );
+});
